f79cc444b576a0e7e9020fbdf6cb995464de1d9f
堀川洋佑 <horikawayouyuu@horikawayouyuunoMacBook-Air.local>
2019-03-05 00:14:17 +0900

    lib実装

--- lib/detect_deprecation.rb
+++ lib/detect_deprecation.rb
@@ -1,6 +1,18 @@
 require "detect_deprecation/version"
 
-module DetectDeprecation
-  class Error < StandardError; end
-  # Your code goes here...
+module ActiveSupport
+  class Deprecation
+    module Reporting
+      def warn(message = nil, callstack = nil)
+        return if silenced
+
+        callstack ||= caller_locations(2)
+        deprecation_message(callstack, message).tap do |m|
+          behavior.each { |b| b.call(m, callstack, deprecation_horizon, gem_name) }
+        end
+
+        raise
+      end
+    end
+  end
 end
